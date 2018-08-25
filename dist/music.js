f117cb3a852152f4020866f40be118a07b2e4e66
xxin <31721634+YoonaXxin@users.noreply.github.com>
2018-08-25 10:38:54 +0800

    Site updated: 2018-08-25 10:38:51

--- dist/music.js
+++ dist/music.js
@@ -3,5 +3,12 @@ const ap = new APlayer({
     fixed: true,
     autoplay: false,
     audio: [
+       {
+        name: '红模仿',
+        artist: '周杰伦',
+        url: 'http://other.web.ra01.sycdn.kuwo.cn/resource/n3/128/92/5/3756663247.mp3',
+        cover: 'http://singerimg.kugou.com/uploadpic/softhead/150/20180515/20180515002522714.jpg',
+      }
+      
     ]
 });
